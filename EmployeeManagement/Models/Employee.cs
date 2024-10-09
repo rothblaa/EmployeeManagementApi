cd3baddf290b0715264456d876a7bef559574263
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-09 18:41:06 -0500

    validation with data annotations

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,12 +1,25 @@
-﻿namespace EmployeeManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
 {
     public class Employee
     {
-        public required int Id { get; set; }
-        public required string FirstName { get; set; }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
         public required string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
         public required string Phone { get; set; }
+
+        [Required(ErrorMessage = "Position is required")]
         public required string Position { get; set; }
     }
 }
