9596eb8fd6f0d7e7ec798faec25abbcf8651e552
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-08 21:43:02 -0500

    Add project files.

---
+++ EmployeeManagement/Repositories/EmployeeRepository.cs
@@ -0,0 +1,51 @@
+﻿using EmployeeManagement.Data;
+using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext _context;
+        public EmployeeRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //always double-check async isn't asynch!!!
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+
+            //whenever you implement logic, call SaveChanges()
+            await _context.SaveChangesAsync();
+
+        }
+
+        public async Task DeleteEmployeeAsync(int id)
+        {
+            var employeeInDb = await _context.Employees.FindAsync(id);
+            if (employeeInDb == null) {
+                throw new KeyNotFoundException($"Employee with id {id} was not found.");
+            }
+
+            _context.Employees.Remove(employeeInDb);
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllAsync()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<Employee?> GetByIdAsync(int? id)
+        {
+            return await _context.Employees.FindAsync(id);
+        }
+
+        public async Task UpdateEmployeeAsync(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
